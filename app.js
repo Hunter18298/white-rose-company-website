c4770ad68a835119a5da0cac1ff20df22109f8c6
Hunter18298 <aland.goran182@gmail.com>
2022-11-26 07:02:31 +0300

    done need feedback

--- app.js
+++ app.js
@@ -27,3 +27,37 @@ window.addEventListener('DOMContentLoaded',()=>{
 		},2300)
 	})
 })
+
+
+
+// count
+
+
+let counter=document.querySelectorAll('.count');
+const speed=200;
+window.addEventListener('load',(Event)=>{
+	let intObserver=new IntersectionObserver(entries=>{
+		entries.forEach(entry=>{
+			if(entry.isIntersecting){
+	 counter.forEach(counter=>{
+var updateCount=()=>{
+		const target=+counter.getAttribute('data-bs-target');
+	const count=+counter.innerText;
+	const increments=target/speed;
+	console.log(increments)
+	if(count<target){
+counter.innerText=Math.ceil(count+increments);
+setTimeout(updateCount, 30);
+	}else{
+		counter.innerText=target;
+	}
+}
+updateCount();
+})
+			}
+		})
+	})
+	counter.forEach((counter)=>{
+		intObserver.observe(counter);
+	})
+})
